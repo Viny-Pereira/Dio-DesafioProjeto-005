886ad73ffee4b07ebd9737525a577d1a06d5d095
Viny-Pereira <vinypereira75@gmail.com>
2023-03-11 21:23:22 -0300

    base project

---
+++ src/main/java/me/dio/ecommerce/h2ecommerce/H2EcommerceApplication.java
@@ -0,0 +1,13 @@
+package me.dio.ecommerce.h2ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/ecommerce/h2ecommerce/H2EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.ecommerce.h2ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
